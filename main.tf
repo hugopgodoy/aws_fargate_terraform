61305b4d6214bb25824e28ca1678fa5079b98034
Hugo Godoy <hgodoy@ciandt.com>
2020-01-15 17:25:00 -0300

    db && spread subnets

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # group polices:
 # AmazonEC2ContainerRegistryPowerUser (acesso EC2) ou PowerUserAccess (acesso local)
 
-variable "availability_zone_west" {
+variable "availability_zone_east_b" {
   type    = string
-  default = "us-west-1a"
+  default = "us-east-1b"
 }
 
 variable "availability_zone_east" {
@@ -15,7 +15,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "poc-vpc-01" {
+resource "aws_vpc" "poc_vpc_01" {
   assign_generated_ipv6_cidr_block = false
   cidr_block                       = "10.0.0.0/16"
   enable_dns_support               = true
@@ -40,30 +40,36 @@ resource "aws_vpc" "poc-vpc-01" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.poc-vpc-01.id
+resource "aws_internet_gateway" "poc_gw" {
+  vpc_id = aws_vpc.poc_vpc_01.id
 }
 
-# variable "vpc_id" {}
-
-data "aws_vpc" "selected" {
-  id = aws_vpc.poc-vpc-01.id
-}
-
-resource "aws_subnet" "poc-subnet-public-01" {
-  vpc_id            = data.aws_vpc.selected.id
+resource "aws_subnet" "poc_subnet_east_01" {
+  vpc_id            = aws_vpc.poc_vpc_01.id
   availability_zone = var.availability_zone_east
-  cidr_block        = cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)
+  # cidr_block        = cidrsubnet(aws_vpc.poc_vpc_01.cidr_block, 4, 1)
+  cidr_block = "10.0.0.0/24"
 
   tags = {
-    "Name" = "poc-subnet-public-01"
+    "Name" = "poc-subnet-east-01"
+  }
+}
+
+resource "aws_subnet" "poc_subnet_east_02" {
+  vpc_id            = aws_vpc.poc_vpc_01.id
+  availability_zone = var.availability_zone_east_b
+  # cidr_block        = cidrsubnet(aws_vpc.poc_vpc_01.cidr_block, 4, 1)
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    "Name" = "poc-subnet-west-01"
   }
 }
 
 resource "aws_security_group" "allow_db_access" {
-  name        = "allow_db_access"
+  name        = "allow-db-access"
   description = "Allow postgres inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = aws_vpc.poc_vpc_01.id
 
   ingress {
     from_port   = 5432
@@ -91,9 +97,9 @@ resource "aws_security_group" "allow_db_access" {
 
 
 resource "aws_security_group" "allow_net_access" {
-  name        = "allow_net_access"
+  name        = "allow-net-access"
   description = "Allow internet inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = aws_vpc.poc_vpc_01.id
 
   ingress {
     from_port   = 80
@@ -131,9 +137,9 @@ resource "aws_security_group" "allow_net_access" {
 }
 
 resource "aws_security_group" "allow_ssh_access" {
-  name        = "allow_ssh_access"
+  name        = "allow-ssh-access"
   description = "Allow ssh inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = aws_vpc.poc_vpc_01.id
 
   ingress {
     from_port   = 22
@@ -161,67 +167,67 @@ resource "aws_security_group" "allow_ssh_access" {
 
 
 
-resource "aws_instance" "poc-ec2-01" {
+resource "aws_instance" "poc_ec2_01" {
   ami             = "ami-b374d5a5" # us-east-1
   instance_type   = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.allow_net_access.id, aws_security_group.allow_ssh_access.id, aws_security_group.allow_db_access.id]
 
-  subnet_id = aws_subnet.poc-subnet-public-01.id
+  subnet_id = aws_subnet.poc_subnet_east_01.id
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.poc-ec2-01.public_ip} > ip_address.txt"
+    command = "echo ${aws_instance.poc_ec2_01.public_ip} > ip_address.txt"
   }  
 }
 
-
-# resource "aws_network_interface" "multi-ip" {
-#   subnet_id   = aws_subnet.poc-subnet-public-01.id
+# resource "aws_network_interface" "multi_ip" {
+#   subnet_id   = aws_subnet.poc_subnet_east_01.id
 #   # private_ips = ["10.0.0.10", "10.0.0.11"]
 # }
 
-resource "aws_eip" "one" {
+resource "aws_eip" "poc_eip_01" {
   vpc                       = true
-  # network_interface         = aws_network_interface.multi-ip.id
-  instance                  = aws_instance.poc-ec2-01.id
-  depends_on                = [aws_internet_gateway.gw]
+  # network_interface         = aws_network_interface.multi-ip.id # instancia OU interface
+  instance                  = aws_instance.poc_ec2_01.id
+  depends_on                = [aws_internet_gateway.poc_gw]
   # associate_with_private_ip = "10.0.0.10"
 }
 
 
+resource "aws_db_subnet_group" "poc_dbsubnetgroup_01" {
+  name       = "poc-dbsubnetgroup-01"
+  subnet_ids = [aws_subnet.poc_subnet_east_01.id, aws_subnet.poc_subnet_east_02.id]
+
+  tags = {
+    Name = "POC RDS subnet group"
+  }
+}
 
 
+resource "aws_db_instance" "poc_db_01" {
+  identifier                = "poc-db-01"
+  availability_zone         = var.availability_zone_east
+  engine                    = "postgres"
+  engine_version            = "11.5"
+  instance_class            = "db.t2.micro"
+  allocated_storage         = 20
 
+  name                      = "poc_demo"
+  username                  = "poc_user"
+  password                  = "ci&t2020"
+  port                      = "5432"
 
+  storage_encrypted         = false
+  deletion_protection       = false
 
+  # final_snapshot_identifier = true
+  skip_final_snapshot       = true
 
+  vpc_security_group_ids    = [aws_security_group.allow_db_access.id]
+  db_subnet_group_name      = aws_db_subnet_group.poc_dbsubnetgroup_01.name
 
-
-# resource "aws_db_instance" "poc-db-01" {
-#   identifier                = "poc-db-01"
-#   availability_zone         = var.availability_zone_east
-#   engine                    = "postgres"
-#   engine_version            = "11.5"
-#   instance_class            = "db.t2.micro"
-#   allocated_storage         = 20
-
-#   name                      = "poc_demo"
-#   username                  = "poc_user"
-#   password                  = "ci&t2020"
-#   port                      = "5432"
-
-#   storage_encrypted         = false
-#   deletion_protection       = false
-
-#   # final_snapshot_identifier = true
-#   skip_final_snapshot       = true
-
-#   vpc_security_group_ids    = [aws_security_group.allow_db_access.id]
-#   # vpc ????
-  # db_subnet_group_name      = data.aws_subnet.selected.id # ????????
-
-#   tags = {
-#     Owner       = "user"
-#     Environment = "demo"
-#   }
-# }
+  tags = {
+    Owner       = "user"
+    Environment = "demo"
+  }
+}
